d3ffcb0314595a3ccdb283086d568d0428b90d4b
ajayreddy9454-cmy <ajayreddy9454@gmail.com>
2026-07-26 21:57:22 +0530

    Configure Aiven MySQL for Render deployment

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,3 +17,27 @@ class Config:
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # ---- Aiven MySQL SSL Support ----
+    # If DB_SSL_CA is set, configure SSL for the MySQL connection.
+    #   - If it points to an existing file, use it directly.
+    #   - Otherwise treat it as the PEM content itself (handy for Render
+    #     where you can paste the CA certificate content into an env var).
+    _ssl_ca = os.getenv("DB_SSL_CA")
+    if _ssl_ca:
+        if os.path.isfile(_ssl_ca):
+            _ca_path = _ssl_ca
+        else:
+            # Write the PEM content to a temporary file
+            _ca_path = os.path.join(tempfile.gettempdir(), "aiven_ca.pem")
+            with open(_ca_path, "w", encoding="utf-8") as _f:
+                _f.write(_ssl_ca)
+
+        SQLALCHEMY_ENGINE_OPTIONS = {
+            "connect_args": {
+                "ssl": {
+                    "ca": _ca_path,
+                }
+            }
+        }
+    # ---- End SSL Support ----
